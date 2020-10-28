c3554b2156e35909a16a586fe3bf46f77e6ba5dd
sayurrr <zredhard@gmail.com>
2020-10-28 18:29:37 +0800

    integrated with hosting API

--- src/functions/universal.js
+++ src/functions/universal.js
@@ -1,5 +1,5 @@
 
-export let baseURL = "http://127.0.0.1";
+export let baseURL = "https://lumeramanado.com";
 
 // generate random number id
 // return INT - Random Number between 1000000 to 9999999
